7f4f8cffe2ffc630a25d2e5e9d78d5ca0969eb77
Balaguru <balagurusiva2002@gmail.com>
2023-08-07 12:57:39 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -8,16 +8,24 @@ let b = ""
 let op = ""
 
 theme1.onclick = function () {
-  document.documentElement.style.cssText = "--bg:white"
+  theme2.setAttribute("style","background-color:inherit")
+  theme3.setAttribute("style","background-color:inherit")
+  theme1.setAttribute("style","background-color:hsl(25, 98%, 40%)")
+  document.documentElement.style.cssText = "--main-background:hsl(222, 26%, 31%);--keypad-background:  hsl(223, 31%, 20%);--toggle-background:  hsl(223, 31%, 20%);--screen-background: hsl(224, 36%, 15%);--key-color: hsl(30, 25%, 89%);--key-shadow:  hsl(28, 16%, 65%);--key-font-color:hsl(221, 14%, 31%);--key-hover: hsl(0, 0%, 93%);--key-rd:   hsl(225, 21%, 49%);--key-red-shadow:  hsl(224, 28%, 35%);--key-red-font-color: hsl(0, 0%, 100%);--key-equal: hsl(6, 63%, 50%);--key-equal-shadow:  hsl(6, 70%, 34%);--white:  hsl(0, 0%, 100%);--cal-color:hsl(0, 0%, 100%);--li-color:hsl(25, 98%, 40%);"
 }
 
 theme2.onclick = function () {
-  document.documentElement.style.cssText = "--bg:black"
+  theme2.setAttribute("style","background-color:hsl(25, 98%, 40%)")
+  theme1.setAttribute("style","background-color:inherit")
+  theme3.setAttribute("style","background-color:inherit")
+  document.documentElement.style.cssText = "--main-background: hsl(0, 0%, 90%);--keypad-background: hsl(0, 5%, 81%);--toggle-background: hsl(0, 5%, 81%);--screen-background: hsl(0, 0%, 93%);--key-color: hsl(30, 25%, 89%);--key-shadow:  hsl(28, 16%, 65%);--key-font-color:hsl(60, 10%, 19%);--key-hover: hsl(0, 0%, 93%);--key-rd:   hsl(185, 42%, 37%);--key-red-shadow: hsl(185, 58%, 25%);--key-red-font-color: hsl(45, 7%, 89%);--key-equal:hsl(25, 98%, 40%);--key-equal-shadow: hsl(25, 99%, 27%);--white:  hsl(0, 0%, 100%);--cal-color:hsl(60, 10%, 19%);--li-color:hsl(25, 98%, 40%);"
 }
 
-theme3
-  .onclick = function () {
-    document.documentElement.style.cssText = "--bg:red"
+theme3.onclick = function () {
+  theme3.setAttribute("style","background-color:hsl(177, 92%, 70%)")
+  theme2.setAttribute("style","background-color:inherit")
+  theme1.setAttribute("style","background-color:inherit")
+    document.documentElement.style.cssText = "--main-background: hsl(268, 75%, 9%);--keypad-background:  hsl(268, 71%, 12%);--toggle-background: hsl(268, 71%, 12%);--screen-background: hsl(268, 71%, 12%);--key-color:hsl(268, 47%, 21%);--key-shadow:hsl(290, 70%, 36%);--key-font-color: hsl(52, 100%, 62%);--key-hover:hsl(281, 92%, 19%);--key-rd: hsl(281, 89%, 26%);--key-red-shadow:hsl(285, 91%, 52%);--key-red-font-color: hsl(45, 7%, 89%);--key-equal: hsl(176, 100%, 44%);--key-equal-shadow: hsl(177, 92%, 70%);--white:  hsl(0, 0%, 100%);--cal-color:hsl(52, 100%, 62%);--li-color:hsl(177, 92%, 70%);"
   }
 
 
